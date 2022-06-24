d9fed723420364eda0f55e58f041132ce4c54c12
KrownWealth <abdulramonadeola36@gmail.com>
2022-06-24 04:44:10 -0700

    Portfolio transfer to a new repository

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main_content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main_content").style.marginLeft= "0";
+}
