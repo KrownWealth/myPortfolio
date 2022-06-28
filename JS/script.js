5882ecf5f9d5e4d2db3a7688d49fcd555bf7ae7b
KrownWealth <abdulramonadeola36@gmail.com>
2022-06-28 08:42:11 -0700

    Media Responsivenes set

---
+++ JS/script.js
@@ -0,0 +1,85 @@
+/*=============typing================*/
+var typed = new Typed(".typing",{
+strings:["Web Developer", "Web Designer", "Graphic Designer"],
+typeSpeed:100,
+BackSpeed:60,
+lopp:true
+})
+
+/*=============sidebar================*/
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+    allSection = document.querySelectorAll("section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        
+        const a = navList[i].querySelector("a");
+       a.addEventListener("click", function()
+       {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("back-section");
+        }
+           for(let j=0; j<totalNavList; j++)
+            {
+                if (navList[j].querySelector("a").classList.contains("active"))
+                {
+                    allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+            if(window.innerWidth < 768)
+            sidebarSectionTogglerBtn();
+       })
+       
+    }
+
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+
+    }
+
+    /* linking hire-me to contact page
+    function updateNav(element)
+    {
+        for(let i=0; i<totalNavList; i++)
+        {
+            navList[i].querySelector("a").classList.remove("active");
+            const target = element.getAttribute("href").split("#")[1];
+        }
+    }
+    document.querySelector(".btn hire-me").addEventListener("click", function()
+    {
+       showSection(this);
+       updateNav(this);
+    })
+    */
+
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+        sidebar = document.querySelector(".sidebar");
+        navTogglerBtn.addEventListener("click", () =>
+        {
+            sidebarSectionTogglerBtn();
+        })
+        function sidebarSectionTogglerBtn()
+        {
+            sidebar.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.toggle("open");
+            }
+
+        }
+
